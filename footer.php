068baaa36564d74b07481dc2b5525302371284b5
Rob Yeomans <76974577+YeomeoR@users.noreply.github.com>
2021-07-10 12:30:21 +0100

    Update footer.php

--- footer.php
+++ footer.php
@@ -1,3 +1,3 @@
 <?php 
-echo "<h5><a href='https://www.yeomeo.dev/home_page.php' target='_blank'>Yeomeo.deV</a></h5>"; 
+echo "<h5><a href='https://www.yeomeo.dev' target='_blank'>Yeomeo.deV</a></h5>"; 
     ?>
